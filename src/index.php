27d1be32dcb8b14f39629e37caf7c71e5d5ac927
ALLIAUME Hugo <kocal@live.fr>
2015-10-17 19:34:43 +0200

    Préparation de la structure du projet

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>start.kocal.fr</title>
+        <link rel="stylesheet" href="css/style.min.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
