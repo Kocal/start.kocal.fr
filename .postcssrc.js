69cb815d585518450a715b6794ef4e98a89c82f6
Hugo Alliaume <kocal@live.fr>
2019-05-24 21:39:15 +0200

    setup purgecss

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,5 +1,19 @@
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/**/*.vue',
+  ],
+
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
+
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
-}
+  plugins: [
+    tailwindcss("./tailwind.config.js"),
+    autoprefixer(),
+    ...process.env.NODE_ENV === "production"
+      ? [purgecss]
+      : [],
+  ],
+};
