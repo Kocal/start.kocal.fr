6b993038f8f1ac211d34e60874e92de941fbb856
Hugo Alliaume <kocal@live.fr>
2019-05-25 18:47:40 +0200

    configure purgecss to use public/index.html

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,6 +3,7 @@ const autoprefixer = require("autoprefixer");
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [
     './src/**/*.vue',
+    './public/index.html',
   ],
 
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
