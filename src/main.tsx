50cd2b6e92b4100aa37ab673ee47dc1f67930d8b
Hugo Alliaume <hugo@alliau.me>
2023-07-30 08:57:12 +0200

    chore: update React to v18

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './main.css';
 import App from './App';
 import './registerServiceWorker';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
