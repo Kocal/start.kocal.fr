48428f15159859971f9e676f68a66843e973642f
Hugo Alliaume <hugo@alliau.me>
2023-07-30 09:01:09 +0200

    chore: upgrade Vite, TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import * as path from 'path';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import { generateSW } from 'rollup-plugin-workbox';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react()],
   resolve: {
     alias: {
       '/@/': path.resolve(__dirname, 'src'),
