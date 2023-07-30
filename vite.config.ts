f0646684da27b791ae8d5a123727003e51a9d3a7
Hugo Alliaume <hugo@alliau.me>
2023-07-31 00:00:42 +0200

    chore: switch to @vitejs/plugin-react-swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { generateSW } from 'rollup-plugin-workbox';
 import { defineConfig } from 'vite';
 
