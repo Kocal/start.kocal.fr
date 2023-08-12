80944c8153d97273b97b275f7e8fb2a1ff4fdcb9
Hugo Alliaume <hugo@alliau.me>
2023-08-12 18:28:35 +0200

    imp(backgrounds): convert png to jpg, reduce their sizes

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,5 @@
 // @ts-expect-error Type is incorrect :(
-const backgrounds: Record<string, () => { default: string }> = import.meta.glob('./assets/img/backgrounds/*');
+const backgrounds: Record<string, () => { default: string }> = import.meta.glob('./assets/img/backgrounds/*.jpg');
 
 export async function pickRandomBackground(): Promise<string> {
   const bgs = Object.values(backgrounds);
