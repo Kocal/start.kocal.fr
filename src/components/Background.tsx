07f0fb95d6cb8048e67b847f75f900b9d5f9db68
Hugo Alliaume <hugo@alliau.me>
2023-08-13 19:21:17 +0200

    debug

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,9 +1,7 @@
-import {component$, useSignal, useVisibleTask$} from '@builder.io/qwik';
+import {component$, useVisibleTask$} from '@builder.io/qwik';
 //const backgrounds = import.meta.glob<string>('../assets/img/backgrounds/*.jpg', { import: 'default', eager: true });
 
 export const Background = component$(() => {
-  const background = useSignal<string | null>(null);
-
   useVisibleTask$(() => {
     console.log('1st visible');
   })
@@ -11,9 +9,6 @@ export const Background = component$(() => {
   return (
     <div
       class="h-full w-full fixed -z-2 bg-cover group-hover:blur group-hover:scale-105 transition duration-500"
-      style={{
-        backgroundImage: background.value !== null ? `url(${background.value})` : undefined,
-      }}
     />
   );
 });
