2a287cbf8165bd47b5203327ce3d0c2cf02bf3d3
Hugo Alliaume <hugo@alliau.me>
2023-08-13 19:29:54 +0200

    debug

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,13 +1,21 @@
-import {component$, useVisibleTask$} from '@builder.io/qwik';
+import { component$, useSignal, useTask$ } from '@builder.io/qwik';
+
+const backgrounds = import.meta.glob<string>('../assets/img/backgrounds/*.jpg', { import: 'default', as: 'string' });
 
 export const Background = component$(() => {
-  useVisibleTask$(() => {
-    console.log('1st visible');
-  })
+  const background = useSignal<string | null>(null);
+
+  useTask$(async () => {
+    const randomBackgroundIndex = Math.floor(Math.random() * Object.keys(backgrounds).length);
+    background.value = await Object.values(backgrounds)[randomBackgroundIndex]();
+  });
 
   return (
     <div
       class="h-full w-full fixed -z-2 bg-cover group-hover:blur group-hover:scale-105 transition duration-500"
+      style={{
+        backgroundImage: background.value !== null ? `url(${background.value})` : undefined,
+      }}
     />
   );
 });
